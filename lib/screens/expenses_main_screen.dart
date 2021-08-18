a5c4417782dc98bcabb1d55236e26bbbdff9dcb0
Anton <frostanton09@gmail.com>
2021-08-18 15:41:24 +0300

    Pie chart created

--- lib/screens/expenses_main_screen.dart
+++ lib/screens/expenses_main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:expenses_app/widgets/bottom_navigation_bar.dart';
+import 'package:expenses_app/widgets/charts/custom_pie_chart.dart';
 import 'package:expenses_app/widgets/period/period_options_list.dart';
 import 'package:expenses_app/widgets/period_switching_appbar.dart';
 import 'package:flutter/material.dart';
@@ -16,6 +17,13 @@ class _ExpensesMainScreenState extends State<ExpensesMainScreen> {
       body: Column(
         children: [
           PeriodOptionsList(),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 220,
+            child: CustomPieChart(),
+          )
         ],
       ),
       bottomNavigationBar: CustomBottomNavigationBar(),
---
+++ lib/widgets/charts/custom_pie_chart.dart
@@ -0,0 +1,81 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class CustomPieChart extends StatelessWidget {
+  final List<Map<String, dynamic>> data = [
+    {
+      'color': Color(0xFFA239FF),
+      'value': 25.0,
+    },
+    {
+      'color': Color(0xFF36A3FF),
+      'value': 25.0,
+    },
+    {
+      'color': Color(0xFF41C7A1),
+      'value': 12.0,
+    },
+    {
+      'color': Color(0xFFFF4A66),
+      'value': 25.0,
+    },
+    {
+      'color': Color(0xFFFFC946),
+      'value': 13.0,
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 70),
+            child: Column(
+              children: [
+                Text(
+                  'Expenses',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                Text(
+                  'USD 1010,06',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+                SizedBox(height: 11),
+                Text(
+                  'Budget',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                Text(
+                  'USD 1500',
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                ),
+              ],
+            ),
+          ),
+        ),
+        buildPieChart(borderedChart: false),
+        buildPieChart(borderedChart: true),
+      ],
+    );
+  }
+
+  Widget buildPieChart({bool borderedChart = false}) {
+    return PieChart(PieChartData(
+      borderData: FlBorderData(
+        show: true,
+      ),
+      centerSpaceRadius: borderedChart ? double.infinity : 91,
+      startDegreeOffset: 90,
+      sections: data
+          .map((e) => PieChartSectionData(
+                showTitle: false,
+                value: e['value'],
+                color: borderedChart ? e['color'].withOpacity(0.15) : e['color'],
+                radius: borderedChart ? 30 : 8,
+              ))
+          .toList(),
+    ));
+  }
+}
